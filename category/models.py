c92ecda81971ef0bac589890b424987791b9521d
Guna-7 <dgunadhana@gmail.com>
2025-10-03 21:02:58 +0530

    created a model and modified it for categories

--- category/models.py
+++ category/models.py
@@ -6,6 +6,9 @@ class Category(models.Model):
     slug = models.CharField(max_length=100,unique=True)
     description = models.TextField(max_length=200)
     image = models.ImageField(upload_to='photos/category',blank=True)
-
+    
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
     def __str__(self):
         return self.Category_name
